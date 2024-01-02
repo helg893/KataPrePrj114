06970a0838b9ce53f3fed8830fd0004951efc187
helg893 <helg893@gmail.com>
2024-01-02 10:59:03 +0300

    removed comments

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -7,18 +7,7 @@ import jm.task.core.jdbc.service.UserServiceImpl;
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
-        System.out.println("Hello KATA PreProject! 1.1.4");
 
-//        UserDaoJDBCImpl userDao = new UserDaoJDBCImpl();
-//        userDao.saveUser("Chuck", "Norris", (byte) 35);
-//        userDao.saveUser("Bruce", "Lee", (byte) 35);
-//        userDao.saveUser("Al", "Pachino", (byte) 43);
-//        System.out.println(userDao.getAllUsers());
-
-//        userDao.cleanUsersTable();
-//        System.out.println(userDao.getAllUsers());
-//        userDao.dropUsersTable();
-//        userDao.createUsersTable();
         UserService userService = new UserServiceImpl();
         userService.createUsersTable();
         userService.saveUser("Chuck", "Norris", (byte) 44);
